fc1619ce7b5e3352263eed72cca662191557af0a
abraham otoo <abrahamotoo@abrahams-MacBook-Pro.local>
2019-04-21 11:14:05 +0200

    sass added

---
+++ tourData.js
@@ -0,0 +1,50 @@
+export const tourData = [
+  {
+    id: 1,
+    city: "Accra",
+    img: "./img/Accra.jpeg",
+    name: "accra by night ",
+    info:
+      "he sweeping white-sand crescent of Bondi is one of Australia’s most iconic beaches. Reliable waves draw surfers while, nearby, hardy locals swim in the Icebergs ocean pool year-round. Trendy, health-conscious Sydneysiders head to laid-back cafes around Hall Street, while hip backpackers frequent the area's casual pubs. Walkers and joggers use the clifftop Bondi to Coogee Coast"
+  },
+  {
+    id: 1,
+    city: "AnotherCity",
+    img: "./img/Accra.jpeg",
+    name: "accra by night ",
+    info:
+      "he sweeping white-sand crescent of Bondi is one of Australia’s most iconic beaches. Reliable waves draw surfers while, nearby, hardy locals swim in the Icebergs ocean pool year-round. Trendy, health-conscious Sydneysiders head to laid-back cafes around Hall Street, while hip backpackers frequent the area's casual pubs. Walkers and joggers use the clifftop Bondi to Coogee Coast"
+  },
+  {
+    id: 1,
+    city: "LA",
+    img: "./img/Accra.jpeg",
+    name: "accra by night ",
+    info:
+      "he sweeping white-sand crescent of Bondi is one of Australia’s most iconic beaches. Reliable waves draw surfers while, nearby, hardy locals swim in the Icebergs ocean pool year-round. Trendy, health-conscious Sydneysiders head to laid-back cafes around Hall Street, while hip backpackers frequent the area's casual pubs. Walkers and joggers use the clifftop Bondi to Coogee Coast"
+  },
+  {
+    id: 1,
+    city: "SomeCity",
+    img: "./img/Accra.jpeg",
+    name: "accra by night ",
+    info:
+      "he sweeping white-sand crescent of Bondi is one of Australia’s most iconic beaches. Reliable waves draw surfers while, nearby, hardy locals swim in the Icebergs ocean pool year-round. Trendy, health-conscious Sydneysiders head to laid-back cafes around Hall Street, while hip backpackers frequent the area's casual pubs. Walkers and joggers use the clifftop Bondi to Coogee Coast"
+  },
+  {
+    id: 1,
+    city: "Sydney",
+    img: "./img/Accra.jpeg",
+    name: "accra by night ",
+    info:
+      "he sweeping white-sand crescent of Bondi is one of Australia’s most iconic beaches. Reliable waves draw surfers while, nearby, hardy locals swim in the Icebergs ocean pool year-round. Trendy, health-conscious Sydneysiders head to laid-back cafes around Hall Street, while hip backpackers frequent the area's casual pubs. Walkers and joggers use the clifftop Bondi to Coogee Coast"
+  },
+  {
+    id: 1,
+    city: "Uptown",
+    img: "./img/Accra.jpeg",
+    name: "accra by night ",
+    info:
+      "he sweeping white-sand crescent of Bondi is one of Australia’s most iconic beaches. Reliable waves draw surfers while, nearby, hardy locals swim in the Icebergs ocean pool year-round. Trendy, health-conscious Sydneysiders head to laid-back cafes around Hall Street, while hip backpackers frequent the area's casual pubs. Walkers and joggers use the clifftop Bondi to Coogee Coast"
+  }
+];
